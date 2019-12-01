9ae0f2882a3f9bfcb97d3393c5fd82ba15fba7d3
Kevin Quick <kquick@galois.com>
2019-12-01 00:54:24 -0800

    Update for puzzle2

---
+++ prolog/day1/fuel2.pl
@@ -0,0 +1,29 @@
+file_masses(F, Masses) :-
+    open(F, read, FStrm, []),
+    read_line_to_string(FStrm, Line),
+    mass_line(FStrm, Line, Masses).
+
+mass_line(_, end_of_file, []) :- !.
+mass_line(FStrm, Line, [M|MS]) :-
+    number_string(M, Line),
+    read_line_to_string(FStrm, NewLine),
+    mass_line(FStrm, NewLine, MS).
+
+read_masses(mass(M), FName) :- file_masses(FName, Masses), member(M, Masses).
+
+test() :- fuel_needed("fuelfile.txt", Fuel), Fuel == 51316.
+
+fuel(0, mass(0)) :- !.
+fuel(F, mass(M)) :-
+    MF is max(0, (M // 3) - 2),
+    fuel(FF, mass(MF)),
+    F is MF + FF.
+
+total_fuel(Fuel) :-
+    findall(F, (mass(M), fuel(F, mass(M))), FS),
+    sum_list(FS, Fuel).
+
+fuel_needed(FName, Fuel) :-
+    findall(F, (read_masses(mass(M), FName), fuel(F, mass(M))), FS),
+    sum_list(FS, Fuel).
+
--- prolog/day1/run.sh
+++ prolog/day1/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p swiProlog
 
+echo -n "Puzzle1: "
 swipl -g 'test, fuel_needed("input", Fuel), print(Fuel).' -t halt fuel.pl
 echo
+
+swipl -g 'test, fuel_needed("input", Fuel), format('\''Puzzle2: ~d~n'\'', [Fuel]).' -t halt fuel2.pl
 
